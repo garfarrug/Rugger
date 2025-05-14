10f398cf6664ad0829520f3fccb056f5975c9cdf
garfarrug <garfarbraimah0@gmail.com>
2025-05-14 00:18:24 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,12 +4,12 @@ const chalk = require('chalk')
 //owmner v card
 global.ytname = "YT: ElitePro" //ur yt chanel name
 global.socialm = "GitHub: Elite-MD" //ur github or insta name
-global.location = "Port Harcourt, Nigeria" //ur location
+global.location = "Cape Coast, Ghana" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'Ruger ' //ur bot name
+global.ownernumber = '233541754489 ' //ur owner number
+global.ownername = 'Garfar ' //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
@@ -20,7 +20,7 @@ global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2\n\n+2348109263390"
 global.creator = "2348109263390@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["233541754489 "] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
